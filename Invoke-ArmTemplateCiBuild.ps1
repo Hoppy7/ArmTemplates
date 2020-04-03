058ea95f5ce6cc28b2664a085c03f96c04d1b549
Ross Hopkins <rohopkin@microsoft.com>
2020-04-03 15:08:59 -0700

    refactor

---
+++ Invoke-ArmTemplateCiBuild.ps1
@@ -0,0 +1,67 @@
+[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory = $true)]
+    [string]$resourceGroupName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$location,
+
+    [Parameter(Mandatory = $true)]
+    [string]$path,
+
+    [Parameter(Mandatory = $true)]
+    [string]$paramFileSuffix
+)
+
+function Invoke-ArmTemplateCiBuild {
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$resourceGroupName,
+
+        [Parameter(Mandatory = $true)]
+        [string]$location,
+
+        [Parameter(Mandatory = $true)]
+        [string]$path,
+
+        [Parameter(Mandatory = $true)]
+        [string]$paramFileSuffix
+    )
+
+    #$path = "$(Build.SourcesDirectory)\ArmTemplates";
+    $directory = Get-ChildItem -Path $path -Recurse -Force;
+    $directory = $directory.directory | Select-Object -Unique;
+
+    describe "armTemplateValidation" {
+        foreach ($dir in $directory) {
+
+            $armTemplates = Get-ChildItem -Path $dir.Fullname -Filter "*.json" -Exclude "*$paramFileSuffix*" -Recurse -Depth 0 -Force;
+
+            foreach ($armT in $armTemplates) {
+
+                it "$($armT.Name)" {
+
+                    $deploySplat = @{};
+                    $deploySplat.Add("Mode", "Complete");
+                    $deploySplat.Add("TemplateFile", $armT.FullName);    
+        
+                    $paramfileName = $armT.Name.Substring(0, $armT.Name.LastIndexOf(".")) + ".$paramFileSuffix";
+                    $paramFilePath = $armT.FullName.Replace($armT.Name, $paramfileName);
+                    $paramFile = Test-Path -Path $paramFilePath;
+
+                    if ($paramFile) {
+                        $deploySplat.Add("TemplateParameterFile", $paramFilePath);
+                    }
+
+                    $rg = New-AzResourceGroup -Name $resourceGroupName -Location $location -Force;
+                    $rg | Test-AzResourceGroupDeployment @deploySplat -ErrorAction Stop | Should BeNullOrEmpty;
+                }
+            }
+        }
+    }
+}
+
+Invoke-ArmTemplateCiBuild -resourceGroupName $resourceGroupName -location $location -path $path -paramFileSuffix $paramFileSuffix;
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Azure Resource Manager (ARM) Templates
-
-![Build Status](https://dev.azure.com/rohopkin/SMC/_apis/build/status/Hoppy7.ArmTemplates?branchName=master)
