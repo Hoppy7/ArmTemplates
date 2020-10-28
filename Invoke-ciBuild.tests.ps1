6d105bfa2f512010d64a33360365f4b9663b5284
Ross Hopkins <rohopkin@microsoft.com>
2020-10-28 11:56:29 -0700

    test

--- Invoke-ciBuild.tests.ps1
+++ Invoke-ciBuild.tests.ps1
@@ -33,35 +33,36 @@ function Invoke-ciBuild {
 
     $directory = Get-ChildItem -Path $path -Recurse -Force;
     $uniqueDirs = $directory.directory | ? {$_.FullName -notmatch ".git"} | Select-Object -Unique;
+    $uniqueDirs
 
-    describe "armTemplateValidation" {
+    # describe "armTemplateValidation" {
         
-        foreach ($dir in $uniqueDirs) {
+    #     foreach ($dir in $uniqueDirs) {
 
-            $armTemplates = Get-ChildItem -Path $dir.Fullname -Filter "*.json" -Exclude "*$paramFileSuffix*" -Recurse -Depth 0 -Force;
+    #         $armTemplates = Get-ChildItem -Path $dir.Fullname -Filter "*.json" -Exclude "*$paramFileSuffix*" -Recurse -Depth 0 -Force;
 
-            foreach ($arm in $armTemplates) {
+    #         foreach ($arm in $armTemplates) {
 
-                it "$($arm.Name)" {
+    #             it "$($arm.Name)" {
 
-                    $deploySplat = @{};
-                    $deploySplat.Add("Mode", "Complete");
-                    $deploySplat.Add("TemplateFile", $arm.FullName);    
+    #                 $deploySplat = @{};
+    #                 $deploySplat.Add("Mode", "Complete");
+    #                 $deploySplat.Add("TemplateFile", $arm.FullName);    
         
-                    $paramfileName = $arm.Name.Substring(0, $arm.Name.LastIndexOf(".")) + ".$paramFileSuffix";
-                    $paramFilePath = $arm.FullName.Replace($arm.Name, $paramfileName);
-                    $paramFile = Test-Path -Path $paramFilePath;
+    #                 $paramfileName = $arm.Name.Substring(0, $arm.Name.LastIndexOf(".")) + ".$paramFileSuffix";
+    #                 $paramFilePath = $arm.FullName.Replace($arm.Name, $paramfileName);
+    #                 $paramFile = Test-Path -Path $paramFilePath;
 
-                    if ($paramFile) {
-                        $deploySplat.Add("TemplateParameterFile", $paramFilePath);
-                    }
+    #                 if ($paramFile) {
+    #                     $deploySplat.Add("TemplateParameterFile", $paramFilePath);
+    #                 }
 
-                    $rg = New-AzResourceGroup -Name $resourceGroupName -Location $location -Force;
-                    $rg | Test-AzResourceGroupDeployment @deploySplat -ErrorAction Stop | Should BeNullOrEmpty;
-                }
-            }
-        }
-    }
+    #                 $rg = New-AzResourceGroup -Name $resourceGroupName -Location $location -Force;
+    #                 $rg | Test-AzResourceGroupDeployment @deploySplat -ErrorAction Stop | Should BeNullOrEmpty;
+    #             }
+    #         }
+    #     }
+    # }
 }
 
 Invoke-ciBuild -resourceGroupName $resourceGroupName -location $location -path $path -paramFileSuffix $paramFileSuffix;
